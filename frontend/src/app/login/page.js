6a5566aecf4d5ea8f0659ba7aef5b3172babd8de
LPC243012 LPC243012 <LPC243012@G301-CC0201.local>
2024-08-13 19:08:53 +0800

    hh

--- frontend/src/app/login/page.js
+++ frontend/src/app/login/page.js
@@ -1,6 +1,8 @@
 "use client";
 
 import { GeldIcon } from "@/assets/icons/GeldIcon";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 const { useState } = require("react");
 const { useAuth } = require("../../components/utils/AuthProvider");
@@ -10,34 +12,45 @@ const Login = () => {
   const [password, setPassword] = useState("");
   const { login } = useAuth();
   return (
-    <div className="flex h-[100%] ">
-      <div className="flex flex-1 flex-col py-[276px] px-[200px]">
+    <div className="flex h-screen w-screen ">
+      <div className="flex flex-1 flex-col items-center mt-[276px] ml-[222px] gap-[40px]">
         <GeldIcon />
         <div className="flex flex-col items-center">
-          <h1 className="text-[#0F172A] text-[24px] font-semibold">
+          <h1 className="text-[#0F172A] text-[24px] font-semibold ">
             Welcome Back
           </h1>
           <p className="text-[#334155]">
             Welcome back, Please enter your details
           </p>
         </div>
-        <div>
-          <input
-            placeholder="email"
+        <div className="flex flex-col gap-[16px]">
+          <Input
+            placeholder="Email"
+            className="bg-[#F3F4F6] h-[48px] w-[384px]"
             value={email}
             onChange={(e) => {
               setEmail(e.target.value);
             }}
           />
-          <input
-            placeholder="password"
+          <Input
+            placeholder="Password"
+            className="bg-[#F3F4F6] h-[48px]"
             value={password}
             onChange={(e) => {
               setPassword(e.target.value);
             }}
           />
         </div>
-        <button onClick={() => login(email, password)}>Submit</button>
+        <Button
+          className="bg-[#0166FF] rounded-3xl w-[384px] h-[48px]"
+          onClick={() => login(email, password)}
+        >
+          Log in
+        </Button>
+        <div className="flex gap-4">
+          <p>Don't have account?</p>
+          <p className="text-[#0166FF]">Sign Up</p>
+        </div>
       </div>
       <div className="bg-[#0166FF] flex-1 "></div>
     </div>
